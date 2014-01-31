b5ef7600dfc213f35db4f949220794508b3438c2
afv01437 <afv01437@uikd.com>
2014-01-31 11:06:50 +0700

    simple loop

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
+<?php 
+$start=1;
+if (isset($_GET['start']) && $_GET['start']>=1) {
+	$start=round($_GET['start']);
+}
+$stop=100;
+if (isset($_GET['stop']) && $_GET['stop']>$start) {
+	$stop=round($_GET['stop']);
+}
+
+$result = "Start=$start<br />";
+$result .= "Stop=$stop<br />";
+
+for($i=$start;$i<=$stop;$i++) {
+	$result .= $i." " ;
+}
+
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -17,5 +35,6 @@
     <input type="submit" name="send" id="send" value="Submit" />
   </p>
 </form>
+<?php echo $result ?>
 </body>
 </html>
